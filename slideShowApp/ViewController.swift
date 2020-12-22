be2de01a86c74a6c99212ee5dd1a7803ea701b29
yuto <yuto00711127@ezweb.ne.jp>
2020-12-22 21:11:01 +0900

    Initial Commit

---
+++ slideShowApp/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  slideShowApp
+//
+//  Created by 河崎優人 on 2020/12/22.
+//  Copyright © 2020 yuto. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
